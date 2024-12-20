988009cd04b112455c2c071c226d1d341afeb00b
LUCA-NICOLAE CUCLEA <luca-nicolae.cuclea@s.unibuc.ro>
2024-12-20 21:35:49 +0200

    Adaugare buton catre Carts + CartsController Urmeaza: Carts views + butoane și conditii de add to cart in index si show de la products

---
+++ ArticlesApp/Controllers/CartsController.cs
@@ -0,0 +1,114 @@
+﻿using ArticlesApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticlesApp.Controllers
+{
+    [Authorize]
+    public class CartsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public CartsController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+            db = context;
+
+            _userManager = userManager;
+
+            _roleManager = roleManager;
+        }
+
+        [Authorize(Roles = "User,Colaborator,Admin")]
+        public IActionResult Index()
+        {
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.Message = TempData["message"];
+                ViewBag.Alert = TempData["messageType"];
+            }
+
+            SetAccessRights();
+
+            var carts = from cart in db.Carts.Include("User")
+                               .Where(b => b.UserId == _userManager.GetUserId(User))
+                            select cart;
+
+            ViewBag.Carts = carts;
+
+            return View();
+        }
+
+        [Authorize(Roles = "User,Colaborator,Admin")]
+        public IActionResult Show(int id)
+        {
+            SetAccessRights();
+
+            var carts = db.Carts
+                                  .Include("ProductCarts.Product.Category")
+                                  .Include("ProductCarts.Product.User")
+                                  .Include("User")
+                                  .Where(b => b.Id == id)
+                                  .Where(b => b.UserId == _userManager.GetUserId(User))
+                                  .FirstOrDefault();
+
+            if (carts == null)
+            {
+                TempData["message"] = "Resursa cautata nu poate fi gasita";
+                TempData["messageType"] = "alert-danger";
+                return RedirectToAction("Index", "Articles");
+            }
+
+            return View(carts);
+        }
+
+        [Authorize(Roles = "User,Editor,Admin")]
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "User,Editor,Admin")]
+        public ActionResult New(Cart c)
+        {
+            c.UserId = _userManager.GetUserId(User);
+
+            if (ModelState.IsValid)
+            {
+                db.Carts.Add(c);
+                db.SaveChanges();
+                TempData["message"] = "Cosul a fost adaugat";
+                TempData["messageType"] = "alert-success";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(c);
+            }
+        }
+
+        private void SetAccessRights()
+        {
+            ViewBag.AfisareButoane = false;
+
+            if (User.IsInRole("Editor") || User.IsInRole("User"))
+            {
+                ViewBag.AfisareButoane = true;
+            }
+
+            ViewBag.EsteAdmin = User.IsInRole("Admin");
+
+            ViewBag.UserCurent = _userManager.GetUserId(User);
+        }
+    }
+}
